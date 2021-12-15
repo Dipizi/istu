b964517cbb73e8594161d56acb2c2fb086b59cf5
4lexva <94285978+4lexva@users.noreply.github.com>
2021-12-15 16:14:29 +0400

    -

--- py/container.py
+++ py/container.py
@@ -3,7 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog
 from control import *
 from data_table import *
-from ml_controller import MLController
 
 class Container(Frame):
     control = None
@@ -11,7 +10,6 @@ class Container(Frame):
     forecast = None
     forecast_btn = None
     export_btn = None
-    mlController = None
 
     def __init__(self, origin: Tk):
         self.__internal_init(origin)
@@ -28,8 +26,6 @@ class Container(Frame):
         self.forecast_btn.pack()
         self.export_btn = ttk.Button(self.forecast, text = "Экспортировать таблицу", command = self.save_table_as_excel)
         self.export_btn.place(x = 0, y = 0)
-
-        self.mlController = MLController()
 
     def __internal_init(self, origin: Tk):
         Frame.__init__(self, origin, width = 1000, height = 600)
@@ -48,15 +44,3 @@ class Container(Frame):
                 self.data_table.cur_to_excel(file)
             except:
                 pass
-
-    def do_forecast(self):
-        self.mlController.init_model(self.data_table.get_ml_data(self.data_table.mlConnection))
-        _in: list = self.mlController.get_input(self.data_table.get_ml_data(self.data_table.connection))
-        _out: list = self.mlController.predict_for(_in)
-
-        for i in range(0, _in.__len__()):
-            self.data_table.connection.execute("""
-                UPDATE AcademicPerformance
-                SET status = '%s'
-                WHERE student_id = '%s'
-            """, (str(_out[i]), str(_in[i][0])))
